97b00b9a22bcd2567af3b9e4dd9e9529862fbb53
Yati2 <20028056@myrp.edu.sg>
2022-05-19 22:44:36 +0800

    activity lifecycle

--- app/src/main/java/sg/edu/rp/c346/id20028056/demo_riddle/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20028056/demo_riddle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("MainActivity", "onCreate() called.");
 
         btnReveal_Q1=findViewById(R.id.btnReveal_Q1);
         btnReveal_Q2=findViewById(R.id.btnReveal_Q2);
@@ -40,4 +41,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart() called.");
+        super.onRestart();
+    }
+
 }
